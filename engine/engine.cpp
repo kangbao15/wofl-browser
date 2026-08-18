a45b4c8a005a1a90088bdb5ac26c9b01eabf0622
KangBao15 <phamnambac2715@gmail.com>
2026-08-18 09:45:34 +0000

    Add Wofl Engine foundation

---
+++ engine/engine.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class WoflEngine {
+public:
+    std::string getName() const {
+        return "Wofl Engine";
+    }
+
+    std::string getVersion() const {
+        return "0.1.0";
+    }
+};
