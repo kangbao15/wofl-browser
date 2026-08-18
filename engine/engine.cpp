bf708ba39c9661f11c70fd4e1ad97c152c0e1d18
KangBao15 <phamnambac2715@gmail.com>
2026-08-18 10:28:31 +0000

    Add DOM text nodes

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -3,7 +3,14 @@
 #include <vector>
 #include <stack>
 
+enum class WoflNodeType {
+    Document,
+    Element,
+    Text
+};
+
 struct WoflNode {
+    WoflNodeType type;
     std::string tag;
     std::string text;
     std::vector<WoflNode> children;
@@ -19,28 +26,33 @@ public:
         return "0.1.0";
     }
 
-    std::size_t countHTMLTags(const std::string& html) const {
-        std::size_t count = 0;
-
-        for (std::size_t i = 0; i < html.size(); ++i) {
-            if (html[i] == '<' &&
-                i + 1 < html.size() &&
-                html[i + 1] != '/') {
-                ++count;
-            }
-        }
-
-        return count;
+    WoflNode createElement(const std::string& tag) const {
+        return WoflNode{
+            WoflNodeType::Element,
+            tag,
+            "",
+            {}
+        };
     }
 
-    WoflNode createNode(const std::string& tag) const {
-        return WoflNode{tag, "", {}};
+    WoflNode createText(const std::string& text) const {
+        return WoflNode{
+            WoflNodeType::Text,
+            "",
+            text,
+            {}
+        };
     }
 
     WoflNode parseDOM(const std::string& html) const {
-        WoflNode root{"document", "", {}};
-        std::stack<WoflNode*> nodes;
+        WoflNode root{
+            WoflNodeType::Document,
+            "document",
+            "",
+            {}
+        };
 
+        std::stack<WoflNode*> nodes;
         nodes.push(&root);
 
         std::size_t i = 0;
@@ -53,19 +65,17 @@ public:
                     break;
                 }
 
-                std::string tag = html.substr(
-                    i + 1,
-                    end - i - 1
-                );
+                std::string tag =
+                    html.substr(i + 1, end - i - 1);
 
                 if (!tag.empty() && tag[0] == '/') {
                     if (nodes.size() > 1) {
                         nodes.pop();
                     }
-                } else {
-                    WoflNode node{tag, "", {}};
+                } else if (!tag.empty()) {
+                    WoflNode element = createElement(tag);
 
-                    nodes.top()->children.push_back(node);
+                    nodes.top()->children.push_back(element);
 
                     WoflNode* child =
                         &nodes.top()->children.back();
@@ -81,7 +91,15 @@ public:
                     end = html.size();
                 }
 
-                nodes.top()->text += html.substr(i, end - i);
+                std::string text =
+                    html.substr(i, end - i);
+
+                if (!text.empty()) {
+                    nodes.top()->children.push_back(
+                        createText(text)
+                    );
+                }
+
                 i = end;
             }
         }
